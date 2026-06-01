e405127c7082d29c4424211671239c8b1ca04a1b
UmsrAkne <sibata486@gmail.com>
2026-06-02 01:23:32 +0900

    init

---
+++ MusicerChord/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using MusicerChord.Views;
+using Prism.Ioc;
+
+namespace MusicerChord
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+    }
+}
---
+++ MusicerChord/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using MusicerChord.Utils;
+using Prism.Mvvm;
+
+namespace MusicerChord.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        #if DEBUG
+        // ReSharper disable once UnusedMember.Local
+        private readonly string testDirectoryPath =
+            Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
+                @"myFiles\tests\RiderProjects\MusicerChord");
+        #endif
+
+        private readonly AppVersionInfo appVersionInfo = new ();
+
+        public MainWindowViewModel()
+        {
+            SetupDummyData();
+        }
+
+        public string Title => appVersionInfo.Title;
+
+        [Conditional("DEBUG")]
+        private void SetupDummyData()
+        {
+        }
+    }
+}
