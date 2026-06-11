ff4555984369fea994b5a03de9c168c1a6102364
UmsrAkne <sibata486@gmail.com>
2026-06-11 18:43:55 +0900

    feat: Add shuffle sound list functionality
    - Add `ShuffleSoundListCommand` to `SoundListViewModel` for shuffling the sound file order.
    - Update `MainWindow.xaml` to include a button for triggering the shuffle functionality.
    - Use `Guid.NewGuid()` for randomizing sound file order.

--- MusicerChord/ViewModels/SoundListViewModel.cs
+++ MusicerChord/ViewModels/SoundListViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
@@ -53,6 +54,12 @@ namespace MusicerChord.ViewModels
         public DelegateCommand ReverseSoundListCommand => new DelegateCommand(() =>
         {
             var list = SoundFiles.Reverse().ToList();
+            SetUpSoundFilesAndPlaylist(list);
+        });
+
+        public DelegateCommand ShuffleSoundListCommand => new DelegateCommand(() =>
+        {
+            var list = SoundFiles.OrderBy(_ => Guid.NewGuid()).ToList();
             SetUpSoundFilesAndPlaylist(list);
         });
 
